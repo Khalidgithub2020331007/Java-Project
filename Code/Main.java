81da42fa29ce0a1eccf7e2887731e0b7b67c0c77
Sheikh Khalid Azad <107164227+Khalidgithub2020331007@users.noreply.github.com>
2024-07-01 22:08:21 +0600

    Add files via upload

---
+++ Code/Main.java
@@ -0,0 +1,11 @@
+
+import javax.swing.*;
+
+public class Main extends JFrame {
+    public static void main(String[] args) {
+
+        MyFrame frame = new MyFrame();
+        frame.setVisible(true);
+
+    }
+}
